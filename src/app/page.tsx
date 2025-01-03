e7581f625e0c39bca8ec0b5acb73294549603083
Saravanan <saravanan.2110038@srec.ac.in>
2025-01-03 21:39:12 +0530

    made some changes in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,12 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        asdfasd{" "}
+        <p className="text-9xl text-red-400 font-semibold tracking-wide">
+          HI{" "}
+          <span className="italic font-thin bg-gradient-to-b from-cyan-300 to-cyan-700">
+            SARAVANAN
+          </span>
+        </p>
       </main>
     </div>
   );
