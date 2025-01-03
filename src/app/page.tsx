c9d4867f7f221e229bb9280adb25ea30180fbb96
Saravanan <saravanan.2110038@srec.ac.in>
2025-01-03 21:42:04 +0530

    tailwind-css chnges

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <p className="text-9xl text-red-400 font-semibold tracking-wide">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start ">
+        <p className="text-9xl text-red-400 font-semibold tracking-wide w-full">
           HI{" "}
-          <span className="italic font-thin bg-gradient-to-b from-cyan-300 to-cyan-700">
+          <span className="italic font-thin bg-gradient-to-b from-cyan-300 to-cyan-700 w-full text-transparent bg-clip-text p-2">
             SARAVANAN
           </span>
         </p>
